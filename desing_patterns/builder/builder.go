30fc69fc6b3b499737e665d7e0eaaf56dc5098d0
Eugene Nosenko <eugene.nosenko@olx.pl>
2020-10-10 13:04:49 +0200

    feat: initial commit

---
+++ desing_patterns/builder/builder.go
@@ -0,0 +1,67 @@
+package builder
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	indentSize = 2
+)
+
+type HTMLElement struct {
+	name, text string
+	elements   []HTMLElement
+}
+
+type HTMLBuilder struct {
+	rootName string
+	root     HTMLElement
+}
+
+func NewHTMLBuilder(rootName string) *HTMLBuilder {
+	return &HTMLBuilder{
+		rootName,
+		HTMLElement{
+			rootName, "", []HTMLElement{},
+		},
+	}
+}
+
+func (b *HTMLBuilder) String() string {
+	return b.root.String()
+}
+
+func (b *HTMLBuilder) AddChild(childName, childText string) {
+	e := HTMLElement{childName, childText, []HTMLElement{}}
+	b.root.elements = append(b.root.elements, e)
+}
+
+func (b *HTMLBuilder) AddChildFluent(childName, childText string) *HTMLBuilder {
+	e := HTMLElement{childName, childText, []HTMLElement{}}
+	b.root.elements = append(b.root.elements, e)
+
+	return b
+}
+
+func (e *HTMLElement) String() string {
+	return e.string(0)
+}
+
+func (e *HTMLElement) string(indent int) string {
+	sb := strings.Builder{}
+	i := strings.Repeat(" ", indentSize*indent)
+	sb.WriteString(fmt.Sprintf("%s<%s>\n", i, e.name))
+
+	if len(e.text) > 0 {
+		sb.WriteString(strings.Repeat(" ", indentSize*(indent+1)))
+		sb.WriteString(e.text)
+		sb.WriteString("\n")
+	}
+	for _, el := range e.elements {
+		sb.WriteString(el.string(indent + 1))
+	}
+	sb.WriteString(fmt.Sprintf("%s</%s>\n", i, e.name))
+
+	return sb.String()
+}
---
+++ desing_patterns/builder/faceted_builder.go
@@ -0,0 +1,60 @@
+package builder
+
+type Person struct {
+	StreetAddress, Postcode, City string
+	CompanyName, Position         string
+	AnnualIncome                  int
+}
+
+type PersonBuilder struct {
+	person *Person
+}
+
+func NewPersonBuilder() *PersonBuilder {
+	return &PersonBuilder{&Person{}}
+}
+
+type PersonAddressBuilder struct {
+	PersonBuilder
+}
+type PersonJobBuilder struct {
+	PersonBuilder
+}
+
+func (b *PersonBuilder) Lives() *PersonAddressBuilder {
+	return &PersonAddressBuilder{*b}
+}
+
+func (b *PersonBuilder) Works() *PersonJobBuilder {
+	return &PersonJobBuilder{*b}
+}
+
+func (b *PersonAddressBuilder) In(city string) *PersonAddressBuilder {
+	b.person.City = city
+	return b
+}
+
+func (b *PersonAddressBuilder) At(street string) *PersonAddressBuilder {
+	b.person.StreetAddress = street
+	return b
+}
+
+func (b *PersonAddressBuilder) WithPostCode(postCode string) *PersonAddressBuilder {
+	b.person.Postcode = postCode
+	return b
+}
+
+func (b *PersonJobBuilder) At(companyName string) *PersonJobBuilder {
+	b.person.CompanyName = companyName
+	return b
+}
+
+func (b *PersonJobBuilder) Earning(annualIncome int) *PersonJobBuilder {
+	b.person.AnnualIncome = annualIncome
+	return b
+}
+
+func (b *PersonJobBuilder) As(position string) *PersonJobBuilder {
+	b.person.Position = position
+	return b
+}
