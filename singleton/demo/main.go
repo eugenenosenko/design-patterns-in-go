27108ba4c7e1d9d0338fe7fbb9ff572d07085335
Eugene Nosenko <eugenenosenko@gmail.com>
2020-10-15 23:44:26 +0200

    feat: added singleton pattern

---
+++ singleton/demo/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"design-patterns-in-go/singleton"
+	"fmt"
+)
+
+func main() {
+	db := singleton.GetSingletonDatabase()
+	pop := db.GetPopulation("Seoul")
+	fmt.Println("Pop of Seoul = ", pop)
+
+	cities := []string{"Seoul", "Mexico City"}
+	//tp := GetTotalPopulation(cities)
+	tp := singleton.GetTotalPopulationEx(singleton.GetSingletonDatabase(), cities)
+
+	ok := tp == (17500000 + 17400000) // testing on live data
+	fmt.Println(ok)
+
+	names := []string{"alpha", "gamma"} // expect 4
+	tp = singleton.GetTotalPopulationEx(&singleton.DummyDatabase{}, names)
+	ok = tp == 4
+	fmt.Println(ok)
+}
